9277c1a3a8419459075810f8bbcefc4233fd23a8
Matheus Barone <matheusvbarone@gmail.com>
2017-11-20 21:29:58 -0200

    Criando componente de foto

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FotoModule } from './foto/foto.module';
 
 
 @NgModule({
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FotoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
