d9761863850c9e50705540d532d5255daa33c867
Matheus Barone <matheusvbarone@gmail.com>
2018-01-22 10:13:51 -0200

    Dando submit no form

--- project/src/app/cadastro/cadastro.component.ts
+++ project/src/app/cadastro/cadastro.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { FotoComponent } from '../foto/foto.component';
 
 @Component({
     selector: 'cadastro',
-    templateUrl: './cadastro.component.html'
+    templateUrl: './cadastro.component.html',
+    styleUrls: ['./cadastro.component.scss']
 })
 
 export class CadastroComponent{
-    foto: Object = {}
+    foto: FotoComponent = new FotoComponent();
+
+    salvar(evento: Event) {
+        evento.preventDefault();
+        console.log(this.foto);
+    }
 }
--- project/src/app/foto/foto.component.ts
+++ project/src/app/foto/foto.component.ts
@@ -7,6 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 
 export class FotoComponent{
-  @Input() titulo: string;
-  @Input() url: string;
+  @Input() titulo: string  = '';
+  @Input() url: string  = '';
+  descricao: string = '';
 }
